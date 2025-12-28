fef13435c91c754ef9301e75a78e8a33d4e25d41
abubakkar221 <mrabubakkarsheikh@gmail.com>
2025-12-28 13:00:10 +0600

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import joblib
+import numpy as np
 
-st.title("predict study hours")
+st.title("Linear Regression Web Application")
+
+model = joblib.load("linear_marks_model.pkl")
+
+x = st.number_input("Enter value")
+
+if st.button("Predict"):
+    y = model.predict(np.array([[x]]))
+    st.success(f"Prediction: {y[0]}")
