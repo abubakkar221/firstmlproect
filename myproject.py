acd276ed9e4a46fe44a02a98614426d60b95fb98
abubakkar221 <mrabubakkarsheikh@gmail.com>
2025-12-28 13:06:17 +0600

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -4,9 +4,9 @@ import numpy as np
 
 st.title("Linear Regression Web Application")
 
-model = joblib.load("linear_marks_model.pkl")
+model = joblib.load("model.pkl")
 
-x = st.number_input("Enter value")
+x = st.number_input("Enter value", value=0.0)
 
 if st.button("Predict"):
     y = model.predict(np.array([[x]]))
